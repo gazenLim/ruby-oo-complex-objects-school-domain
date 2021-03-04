86a19aa4865b0e88447aa7a144cd8dd317a01040
gajen <meetgajen@yahoo.com>
2021-03-04 16:13:11 -0500

    commit tree 47b123e4cddf674adaf9224cd4a1a83a22549b6a

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+
+class School
+attr_accessor :school, :roster
+
+    def initialize(school,roster={})
+    @school=school
+    @roster=roster
+    end
+
+    def add_student(name,grade)
+        roster[grade]||=[]
+        roster[grade]<<name
+    end
+
+def grade(grades)
+    @roster[grades]
+end
+
+def sort
+    student_sort={}
+    roster.each do |grade,name|
+        student_sort[grade]=name.sort
+
+end
+student_sort
+end
+
+
+
+
+end
